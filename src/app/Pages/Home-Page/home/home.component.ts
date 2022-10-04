4c088f48f99b4035cd7ba41d8da6cfc66913db26
Amr <eng.amrmossad93@hotmail.com>
2022-10-04 11:24:21 +0200

    Cart

--- src/app/Pages/Home-Page/home/home.component.ts
+++ src/app/Pages/Home-Page/home/home.component.ts
@@ -1,6 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {GraphQLService} from "../../../Core/Services/GraphQL/graph-ql.service";
-import {GET_CARTS} from "../../../Core/Models/GraphQL/Queries/GetCarts";
 
 @Component({
   selector: 'app-home',
